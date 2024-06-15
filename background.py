3deb54a6ea3463e44561c8f17eccb44e00fe3143
OscarChen678 <kxchen129@icloud.com>
2024-06-15 17:32:02 +0800

    bg update

--- background.py
+++ background.py
@@ -18,7 +18,6 @@ recbk = 0
 se_crash = None
 mycar = 0
 
-####################road.py####################
 DATA_LR = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 2, 1, 0, 2, 4, 2, 4, 2, 0, 0, 0,-2,-2,-4,-4,-2,-1, 0, 0, 0, 0, 0, 0, 0]
 DATA_UD = [0, 0, 1, 2, 3, 2, 1, 0,-2,-4,-2, 0, 0, 0, 0, 0,-1,-2,-3,-4,-3,-2,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-3, 3, 0,-6, 6, 0]
 CLEN = len(DATA_LR)
@@ -29,22 +28,19 @@ curve = [0]*CMAX
 updown = [0]*CMAX
 object_left = [0]*CMAX
 object_right = [0]*CMAX
-############################
 
-
-####################fig.py####################
 CAR = 30
 car_x = [0]*CAR
 car_y = [0]*CAR
 car_lr = [0]*CAR
 car_spd = [0]*CAR
-PLCAR_Y = 10 # 玩家賽車的顯示位置　道路最前面（畫面下方）為0
+PLCAR_Y = 10
 
 LAPS = 3
 laptime =["0'00.00"]*LAPS
 
 
-def make_course():################# road.py
+def make_course():
     for i in range(CLEN):
         lr1 = DATA_LR[i]
         lr2 = DATA_LR[(i+1)%CLEN]
@@ -55,21 +51,21 @@ def make_course():################# road.py
             curve[pos] = lr1*(BOARD-j)/BOARD + lr2*j/BOARD
             updown[pos] = ud1*(BOARD-j)/BOARD + ud2*j/BOARD
             if j == 60:
-                object_right[pos] = 1 # 看板
+                object_right[pos] = 1 
             if i%8 < 7:
                 if j%12 == 0:
-                    object_left[pos] = 2 # 椰子樹
+                    object_left[pos] = 2 
             else:
                 if j%20 == 0:
-                    object_left[pos] = 3 # 遊艇
+                    object_left[pos] = 3 
             if j%12 == 6:
-                object_left[pos] = 9 # 大海
+                object_left[pos] = 9 
 
 
 def time_str(val):
-    sec = int(val) # 參數變成整數的秒數
-    ms  = int((val-sec)*100) # 小數部分
-    mi  = int(sec/60) # 分
+    sec = int(val)
+    ms  = int((val-sec)*100) 
+    mi  = int(sec/60) 
     return "{}'{:02}.{:02}".format(mi, sec%60, ms)
 
 
@@ -83,13 +79,13 @@ def draw_obj(bg, img, x, y, sc):
 def draw_shadow(bg, x, y, siz):
     shadow = pygame.Surface([siz, siz/4])
     shadow.fill(RED)
-    shadow.set_colorkey(RED) # 設定Surface的透明色
-    shadow.set_alpha(128) # 設定Surface的透明度
+    shadow.set_colorkey(RED) 
+    shadow.set_alpha(128) 
     pygame.draw.ellipse(shadow, BLACK, [0,0,siz,siz/4])
     bg.blit(shadow, [x-siz/2, y-siz/4])
 
 
-def init_car():################# figure.py
+def init_car():
     for i in range(1, CAR):
         car_x[i] = random.randint(50, 750)
         car_y[i] = random.randint(200, CMAX-200)
@@ -101,7 +97,7 @@ def init_car():################# figure.py
     car_spd[0] = 0
 
 
-def drive_car(key): # 操作、控制玩家的賽車######### figure.py
+def drive_car(key): 
     global idx, tmr, laps, recbk
     if key[K_LEFT] == 1:
         if car_lr[0] > -3:
@@ -114,9 +110,9 @@ def drive_car(key): # 操作、控制玩家的賽車######### figure.py
     else:
         car_lr[0] = int(car_lr[0]*0.9)
 
-    if key[K_a] == 1: # 油門
+    if key[K_a] == 1:
         car_spd[0] += 3
-    elif key[K_z] == 1: # 煞車
+    elif key[K_z] == 1:
         car_spd[0] -= 10
     else:
         car_spd[0] -= 0.25
@@ -145,7 +141,7 @@ def drive_car(key): # 操作、控制玩家的賽車######### figure.py
             tmr = 0
 
 
-def move_car(cs): # 控制COM賽車################# figure.py
+def move_car(cs): 
     for i in range(cs, CAR):
         if car_spd[i] < 100:
             car_spd[i] += 3
@@ -163,15 +159,15 @@ def move_car(cs): # 控制COM賽車################# figure.py
         car_y[i] += car_spd[i]/100
         if car_y[i] > CMAX-1:
             car_y[i] -= CMAX
-        if idx == 2: # 賽車中的碰撞偵測
+        if idx == 2:
             cx = car_x[i]-car_x[0]
             cy = car_y[i]-(car_y[0]+PLCAR_Y)%CMAX
             if -100 <= cx and cx <= 100 and -10 <= cy and cy <= 10:
-                # 碰撞時的座標變化、交換速度及減速
+                
                 car_x[0] -= cx/4
                 car_x[i] += cx/4
                 car_spd[0], car_spd[i] = car_spd[i]*0.3, car_spd[0]*0.3
-                se_crash.play()
+                
 
 
 def draw_text(scrn, txt, x, y, col, fnt):
@@ -183,8 +179,8 @@ def draw_text(scrn, txt, x, y, col, fnt):
     scrn.blit(sur, [x, y])
 
 
-def main(): # 主要處理
-    global idx, tmr, laps, rec, recbk, se_crash, mycar
+def main(): 
+    global idx, tmr, laps, rec, recbk, mycar
     pygame.init()
     pygame.display.set_caption("Python Racer")
     screen = pygame.display.set_mode((800, 600))
@@ -226,13 +222,13 @@ def main(): # 主要處理
         pygame.image.load("image_pr/car26.png").convert_alpha()
     ]
 
-    se_crash = pygame.mixer.Sound("sound_pr/crash.ogg") # 載入SE
+    
 
-    # 計算道路板子的基本形狀
+    
     BOARD_W = [0]*BOARD
     BOARD_H = [0]*BOARD
     BOARD_UD = [0]*BOARD
-    for i in range(BOARD):################# road.py
+    for i in range(BOARD):
         BOARD_W[i] = 10+(BOARD-i)*(BOARD-i)/12
         BOARD_H[i] = 3.4*(BOARD-i)/BOARD
         BOARD_UD[i] = 2*math.sin(math.radians(i*1.5))
@@ -254,7 +250,7 @@ def main(): # 主要處理
                     screen = pygame.display.set_mode((800, 600))
         tmr += 1
 
-        # 計算繪製道路用的X座標與路面高低
+    
         di = 0
         ud = 0
         board_x = [0]*BOARD
@@ -265,22 +261,22 @@ def main(): # 主要處理
             board_x[i] = 400 - BOARD_W[i]*car_x[0]/800 + di/2
             board_ud[i] = ud/30
 
-        horizon = 400 + int(ud/3) # 計算地平線的座標
-        sy = horizon # 開始繪製道路的位置
+        horizon = 400 + int(ud/3) 
+        sy = horizon 
 
-        vertical = vertical - int(car_spd[0]*di/8000) # 背景的垂直位置
+        vertical = vertical - int(car_spd[0]*di/8000) 
         if vertical < 0:
             vertical += 800
         if vertical >= 800:
             vertical -= 800
 
-        # 繪製草地
-        screen.fill((0, 56, 255)) # 天空的顏色
+        
+        screen.fill((0, 56, 255)) 
         screen.blit(img_bg, [vertical-800, horizon-400])
         screen.blit(img_bg, [vertical, horizon-400])
-        screen.blit(img_sea, [board_x[BOARD-1]-780, sy]) # 最遠的大海
+        screen.blit(img_sea, [board_x[BOARD-1]-780, sy]) 
 
-        # 根據繪圖用資料繪製道路
+        
         for i in range(BOARD-1, 0, -1):
             ux = board_x[i]
             uy = sy - BOARD_UD[i]*board_ud[i]
@@ -290,38 +286,38 @@ def main(): # 主要處理
             by = sy - BOARD_UD[i-1]*board_ud[i-1]
             bw = BOARD_W[i-1]
             col = (160,160,160)
-            if int(car_y[0]+i)%CMAX == PLCAR_Y+10: # 紅線的位置
+            if int(car_y[0]+i)%CMAX == PLCAR_Y+10:
                 col = (192,0,0)
             pygame.draw.polygon(screen, col, [[ux, uy], [ux+uw, uy], [bx+bw, by], [bx, by]])
 
-            if int(car_y[0]+i)%10 <= 4: # 左右的黃線
+            if int(car_y[0]+i)%10 <= 4:
                 pygame.draw.polygon(screen, YELLOW, [[ux, uy], [ux+uw*0.02, uy], [bx+bw*0.02, by], [bx, by]])
                 pygame.draw.polygon(screen, YELLOW, [[ux+uw*0.98, uy], [ux+uw, uy], [bx+bw, by], [bx+bw*0.98, by]])
-            if int(car_y[0]+i)%20 <= 10: # 白線
+            if int(car_y[0]+i)%20 <= 10:
                 pygame.draw.polygon(screen, WHITE, [[ux+uw*0.24, uy], [ux+uw*0.26, uy], [bx+bw*0.26, by], [bx+bw*0.24, by]])
                 pygame.draw.polygon(screen, WHITE, [[ux+uw*0.49, uy], [ux+uw*0.51, uy], [bx+bw*0.51, by], [bx+bw*0.49, by]])
                 pygame.draw.polygon(screen, WHITE, [[ux+uw*0.74, uy], [ux+uw*0.76, uy], [bx+bw*0.76, by], [bx+bw*0.74, by]])
 
             scale = 1.5*BOARD_W[i]/BOARD_W[0]
-            obj_l = object_left[int(car_y[0]+i)%CMAX] # 道路左邊的物體
-            if obj_l == 2: # 椰子樹
+            obj_l = object_left[int(car_y[0]+i)%CMAX]
+            if obj_l == 2:
                 draw_obj(screen, img_obj[obj_l], ux-uw*0.05, uy, scale)
-            if obj_l == 3: # 遊艇
+            if obj_l == 3:
                 draw_obj(screen, img_obj[obj_l], ux-uw*0.5, uy, scale)
-            if obj_l == 9: # 大海
+            if obj_l == 9:
                 screen.blit(img_sea, [ux-uw*0.5-780, uy])
-            obj_r = object_right[int(car_y[0]+i)%CMAX] # 道路右邊的物體
-            if obj_r == 1: # 看板
+            obj_r = object_right[int(car_y[0]+i)%CMAX]
+            if obj_r == 1:
                 draw_obj(screen, img_obj[obj_r], ux+uw*1.3, uy, scale)
 
-            for c in range(1, CAR): # COM賽車
+            for c in range(1, CAR):
                 if int(car_y[c])%CMAX == int(car_y[0]+i)%CMAX:
-                    lr = int(4*(car_x[0]-car_x[c])/800) # 玩家看到COM賽車的方向
+                    lr = int(4*(car_x[0]-car_x[c])/800)
                     if lr < -3: lr = -3
                     if lr >  3: lr =  3
                     draw_obj(screen, img_car[(c%3)*7+3+lr], ux+car_x[c]*BOARD_W[i]/800, uy, 0.05+BOARD_W[i]/BOARD_W[0])
 
-            if i == PLCAR_Y: # 玩家賽車
+            if i == PLCAR_Y:
                 draw_shadow(screen, ux+car_x[0]*BOARD_W[i]/800, uy, 200*BOARD_W[i]/BOARD_W[0])
                 draw_obj(screen, img_car[3+car_lr[0]+mycar*7], ux+car_x[0]*BOARD_W[i]/800, uy, 0.05+BOARD_W[i]/BOARD_W[0])
 
@@ -354,8 +350,6 @@ def main(): # 主要處理
             n = 3-int(tmr/60)
             draw_text(screen, str(n), 400, 240, YELLOW, fnt_l)
             if tmr == 179:
-                pygame.mixer.music.load("sound_pr/bgm.ogg")
-                pygame.mixer.music.play(-1)
                 idx = 2
                 tmr = 0
 
@@ -367,11 +361,6 @@ def main(): # 主要處理
             move_car(1)
 
         if idx == 3:
-            if tmr == 1:
-                pygame.mixer.music.stop()
-            if tmr == 30:
-                pygame.mixer.music.load("sound_pr/goal.ogg")
-                pygame.mixer.music.play(0)
             draw_text(screen, "GOAL!", 400, 240, GREEN, fnt_l)
             car_spd[0] = car_spd[0]*0.96
             car_y[0] = car_y[0] + car_spd[0]/100
